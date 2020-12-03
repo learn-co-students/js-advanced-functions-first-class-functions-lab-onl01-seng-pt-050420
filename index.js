f8a29c51630f8d25283083ed3e2128d9f2f058dd
danielkwon89 <danielkwon89@gmail.com>
2020-12-02 22:58:20 -0500

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const returnFirstTwoDrivers = (arr) => {
 
 const returnLastTwoDrivers = (arr) => {
     // return [arr[arr.length-2], arr[arr.length-1]]
-    retur arr.slice(-2)
+    return arr.slice(-2)
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
@@ -14,13 +14,17 @@ const createFareMultiplier = (multiplier) => {
     return (fare) => fare * multiplier
 }
 
-const fareDoubler = (fare) => {
-    return createFareMultiplier(2)(fare)
-}
+// const fareDoubler = (fare) => {
+//     return createFareMultiplier(2)(fare)
+// }
 
-const fareTripler = (fare) => {
-    return createFareMultiplier(3)(fare)
-}
+const fareDoubler = createFareMultiplier(2)
+
+// const fareTripler = (fare) => {
+//     return createFareMultiplier(3)(fare)
+// }
+
+const fareTripler = createFareMultiplier(3)
 
 const selectDifferentDrivers = (arr, func) => {
     return func(arr)
