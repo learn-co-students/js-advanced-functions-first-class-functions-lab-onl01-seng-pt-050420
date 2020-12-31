17eff170a68190eb7804c27db1e7bdb57c45ff1d
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-12-31 14:14:38 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr) {
+    return arr.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(arr) {
+    return arr.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(num) { 
+    return function(fare) {
+        return num * fare 
+    }
+}
+
+const fareDoubler = function(num) {
+    return 2 * num 
+}
+
+const fareTripler = function(num) {
+    return 3 * num 
+}
+
+const selectDifferentDrivers = function(arrayofDrivers, func) {
+    return func(arrayofDrivers)
+}
